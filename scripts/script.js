c0bcd63131ce8675c9b72485c12b6db35e41430d
NadimRifaii <nadimrifaii3@gmail.com>
2023-11-15 12:01:11 +0200

    finished the overlay and landing section

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,8 @@
 const bars = document.querySelector('.bars')
+const links = document.querySelector('.links')
 const overlay = document.querySelector('.overlay')
 bars.addEventListener('click', () => {
   overlay.classList.toggle('active')
+  links.classList.toggle('active')
+  document.body.classList.toggle('active')
 })
