dede6ab30f424fc6f5931134ae6d491a18e302d2
NadimRifaii <nadimrifaii3@gmail.com>
2023-11-16 16:51:59 +0200

    finished the hire talent section

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,9 @@ const bars = document.querySelector('.bars')
 const links = document.querySelector('.links')
 const overlay = document.querySelector('.overlay')
 const programs = document.querySelector('.programs')
+const slideItems = document.querySelectorAll('.person')
 const options = document.querySelectorAll('.programs ul li')
+const slideControls = document.querySelectorAll('.slider-controls span')
 const programElements = document.querySelectorAll('.programs .program')
 const programsBackgroundClasses = ['#ffc635', '#28eea7', '#9864da', '#fb508e']
 bars.addEventListener('click', () => {
@@ -13,15 +15,34 @@ bars.addEventListener('click', () => {
 
 options.forEach((option, index) => {
   option.addEventListener('click', () => {
-    options.forEach((op, index) => {
-      removeActiveFromElement(op)
-      removeActiveFromElement(programElements[index])
-    })
+    removeActiveFromElements(options)
+    removeActiveFromElements(programElements)
     option.classList.toggle('active')
     programElements[index].classList.toggle('active')
     programs.style.backgroundColor = programsBackgroundClasses[index]
   })
 })
-function removeActiveFromElement(element) {
-  element.classList.remove('active')
+function removeActiveFromElements(elements) {
+  elements.forEach(element => {
+    element.classList.remove('active')
+  })
 }
+slideControls.forEach((control, index) => {
+  control.addEventListener('click', () => {
+    removeActiveFromElements(slideControls)
+    removeActiveFromElements(slideItems)
+    control.classList.toggle('active')
+    slideItems[index].classList.toggle('active')
+  })
+})
+function getRandomIndex() {
+  const random = Math.floor(Math.random() * 4)
+  removeActiveFromElements(slideControls)
+  removeActiveFromElements(slideItems)
+  return random
+}
+setInterval(() => {
+  let index = getRandomIndex()
+  slideControls[index].classList.toggle('active')
+  slideItems[index].classList.toggle('active')
+}, 4000)
