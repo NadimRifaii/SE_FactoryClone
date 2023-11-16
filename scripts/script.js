594cbca7dda14cf2de1d8c747ddd23c10afbe8c8
NadimRifaii <nadimrifaii3@gmail.com>
2023-11-16 20:26:44 +0200

    modified the css for testimonials section and cleaned the script code

--- scripts/script.js
+++ scripts/script.js
@@ -4,37 +4,17 @@ const overlay = document.querySelector('.overlay')
 const programs = document.querySelector('.programs')
 const slideItems = document.querySelectorAll('.person')
 const options = document.querySelectorAll('.programs ul li')
-const slideControls = document.querySelectorAll('.slider-controls span')
 const programElements = document.querySelectorAll('.programs .program')
+const slideControls = document.querySelectorAll('.slider-controls span')
 const programsBackgroundClasses = ['#ffc635', '#28eea7', '#9864da', '#fb508e']
-bars.addEventListener('click', () => {
-  overlay.classList.toggle('active')
-  links.classList.toggle('active')
-  document.body.classList.toggle('active')
-})
-
-options.forEach((option, index) => {
-  option.addEventListener('click', () => {
-    removeActiveFromElements(options)
-    removeActiveFromElements(programElements)
-    option.classList.toggle('active')
-    programElements[index].classList.toggle('active')
-    programs.style.backgroundColor = programsBackgroundClasses[index]
-  })
-})
+function toggleClass(element, clas) {
+  element.classList.toggle(clas)
+}
 function removeActiveFromElements(elements) {
   elements.forEach(element => {
     element.classList.remove('active')
   })
 }
-slideControls.forEach((control, index) => {
-  control.addEventListener('click', () => {
-    removeActiveFromElements(slideControls)
-    removeActiveFromElements(slideItems)
-    control.classList.toggle('active')
-    slideItems[index].classList.toggle('active')
-  })
-})
 function getRandomIndex() {
   const random = Math.floor(Math.random() * 4)
   removeActiveFromElements(slideControls)
@@ -43,6 +23,28 @@ function getRandomIndex() {
 }
 setInterval(() => {
   let index = getRandomIndex()
-  slideControls[index].classList.toggle('active')
-  slideItems[index].classList.toggle('active')
-}, 4000)
+  toggleClass(slideControls[index], 'active')
+  toggleClass(slideItems[index], 'active')
+}, 5000)
+bars.addEventListener('click', () => {
+  toggleClass(overlay, 'active')
+  toggleClass(links, 'active')
+  toggleClass(document.body, 'active')
+})
+slideControls.forEach((control, index) => {
+  control.addEventListener('click', () => {
+    removeActiveFromElements(slideControls)
+    removeActiveFromElements(slideItems)
+    toggleClass(control, 'active')
+    toggleClass(slideItems[index], 'active')
+  })
+})
+options.forEach((option, index) => {
+  option.addEventListener('click', () => {
+    removeActiveFromElements(options)
+    removeActiveFromElements(programElements)
+    toggleClass(option, 'active')
+    toggleClass(programElements[index], 'active')
+    programs.style.backgroundColor = programsBackgroundClasses[index]
+  })
+})
